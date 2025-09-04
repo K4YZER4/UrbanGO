7d86319486976869a71b2b678746b755284e080d
K4YZER4 <horachioxbox@gmail.com>
2025-09-04 12:13:29 -0700

    Se añadio backend y frontend y se arreglaron errores

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("¡Hola, Mundo!2");
+  res.send("¡Hola, Mujytdjtdtrxtrd!");
 });
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
